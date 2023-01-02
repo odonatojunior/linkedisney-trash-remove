48f5808a31cbd1b217d860ec01d5f2dd1357c5a6
Donato Jr <donato.junior.07@gmail.com>
2023-01-02 17:43:23 -0300

    first commit

---
+++ vite.config.ts
@@ -0,0 +1,21 @@
+// @ts-nocheck
+import { resolve } from "path";
+import { defineConfig } from "vite";
+import { chromeExtension } from "vite-plugin-chrome-extension";
+
+export default defineConfig({
+    resolve: {
+        alias: {
+            "@": resolve(__dirname, "src"),
+        },
+    },
+    build: {
+        rollupOptions: {
+            input: "src/manifest.json"
+        },
+        outDir: 'extension'
+    },
+    plugins: [
+        chromeExtension()
+    ],
+})
